dcb8b38adac00fa03b57fa28300d739c147a7d02
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2020-02-29 19:14:28 +0000

    filters moved to separate file

--- main.go
+++ main.go
@@ -79,29 +79,3 @@ func main() {
 	checkErr(err)
 
 }
-
-func reflectFilter(height, width int, image [][]byte) []byte {
-	newImage := make([]byte, 0)
-	for i := 0; i < height; i++ {
-		bytesInRow := width * 3
-		for j := 0; j < bytesInRow/2; j++ {
-			image[i][j], image[i][bytesInRow-1-j] = image[i][bytesInRow-1-j], image[i][j]
-		}
-		for k := 0; k <= bytesInRow-3; k += 3 {
-			newImage = append(newImage, image[i][k+2], image[i][k+1], image[i][k])
-		}
-	}
-	return newImage
-}
-
-func greyScaleFilter(height, width int, image [][]byte) []byte {
-	newImage := make([]byte, 0)
-	for i := 0; i < height; i++ {
-		bytesInRow := width * 3
-		for k := 0; k <= bytesInRow-3; k += 3 {
-			avg := (image[i][k+2] + image[i][k+1] + image[i][k]) / 3
-			newImage = append(newImage, avg, avg, avg)
-		}
-	}
-	return newImage
-}
