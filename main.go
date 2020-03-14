460d520f537f3cbeb948094240421f2249e9c224
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2020-03-14 13:48:30 +0000

    wip

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -11,16 +12,26 @@ func checkErr(err error) {
 	}
 }
 
+type Pixel struct {
+	b, g, r byte
+}
+
 func main() {
 
-	if len(os.Args[1:]) < 3 {
-		fmt.Println("incorrect args provided")
-		os.Exit(1)
+	if err := os.Mkdir("filtered-images", os.FileMode(0777)); os.IsExist(err) {
+		fmt.Println("directory already exists")
 	}
+	// if len(os.Args[1:]) < 3 {
+	// 	fmt.Println("incorrect args provided")
+	// 	os.Exit(1)
+	// }
 
-	filter := os.Args[1]
-	fileName := os.Args[2]
-	newFileName := os.Args[3]
+	// filter := os.Args[1]
+	// fileName := os.Args[2]
+	// newFileName := os.Args[3]
+	// filter := os.Args[1]
+	fileName := "./images/courtyard.bmp"
+	newFileName := "./filtered-images/g-courtyard.bmp"
 
 	file, err := os.Open(fileName)
 	checkErr(err)
@@ -39,37 +50,56 @@ func main() {
 		os.Exit(1)
 	}
 
-	image := make([][]byte, bitMapInfoHeader.pHeight)
+	// collect bytes into pixels
+	image := make([]Pixel, 0)
+	offset := int64(bitMapHeader.startingAddress)
 
-	// width 600pixels.
-	// each pixel made of 24 bits or 3 bytes.
-	// each row is 600 * 3 = 1800 bytes
-	// we have 400 pixels in height. so 400 * 1800 = 720000 bytes
-
-	var startingOffset uint32 = bitMapHeader.startingAddress
-
-	for i := range image {
-		var rowBytes uint32 = bitMapInfoHeader.pWidth * 3
-		data := make([]byte, rowBytes)
-		_, err := file.ReadAt(data, int64(startingOffset))
+	for {
+		bytesRead := make([]byte, 3)
+		n, err := file.ReadAt(bytesRead, offset)
+		if err == io.EOF {
+			fmt.Println("image data read")
+			break
+		}
 		checkErr(err)
-		image[i] = append(image[i], data...)
-		startingOffset += rowBytes
+		image = append(image, Pixel{bytesRead[0], bytesRead[1], bytesRead[2]})
+		offset += int64(n)
 	}
 
-	var newImageData []byte
+	newPixels := greyScaleFilter(int(bitMapInfoHeader.pHeight), int(bitMapInfoHeader.pWidth), image)
+	newImageData := pixelsToBytes(newPixels)
 
-	switch filter {
-	case "-r":
-		newImageData = reflectFilter(int(bitMapInfoHeader.pHeight), int(bitMapInfoHeader.pWidth), image)
-	case "-g":
-		newImageData = greyScaleFilter(int(bitMapInfoHeader.pHeight), int(bitMapInfoHeader.pWidth), image)
-	case "-b":
-		newImageData = blurFilter(int(bitMapInfoHeader.pHeight), int(bitMapInfoHeader.pWidth), image)
-	default:
-		fmt.Printf("filter %s does not exist\n", filter)
-		os.Exit(1)
-	}
+	// image := make([][]byte, bitMapInfoHeader.pHeight)
+
+	// // width 600pixels.
+	// // each pixel made of 24 bits or 3 bytes.
+	// // each row is 600 * 3 = 1800 bytes
+	// // we have 400 pixels in height. so 400 * 1800 = 720000 bytes
+
+	// var startingOffset uint32 = bitMapHeader.startingAddress
+
+	// for i := range image {
+	// 	var rowBytes uint32 = bitMapInfoHeader.pWidth * 3
+	// 	data := make([]byte, rowBytes)
+	// 	_, err := file.ReadAt(data, int64(startingOffset))
+	// 	checkErr(err)
+	// 	image[i] = append(image[i], data...)
+	// 	startingOffset += rowBytes
+	// }
+
+	// var newImageData []byte
+
+	// switch filter {
+	// case "-r":
+	// 	newImageData = reflectFilter(int(bitMapInfoHeader.pHeight), int(bitMapInfoHeader.pWidth), image)
+	// case "-g":
+	// 	newImageData = greyScaleFilter(int(bitMapInfoHeader.pHeight), int(bitMapInfoHeader.pWidth), image)
+	// case "-b":
+	// 	newImageData = blurFilter(int(bitMapInfoHeader.pHeight), int(bitMapInfoHeader.pWidth), image)
+	// default:
+	// 	fmt.Printf("filter %s does not exist\n", filter)
+	// 	os.Exit(1)
+	// }
 
 	newImageBytes := append(bitMapHeader.data, bitMapInfoHeader.data...)
 	newImageBytes = append(newImageBytes, newImageData...)
@@ -79,5 +109,11 @@ func main() {
 
 	_, err = newFile.Write(newImageBytes)
 	checkErr(err)
+}
 
+func pixelsToBytes(pixels []Pixel) (bytes []byte) {
+	for _, pixel := range pixels {
+		bytes = append(bytes, pixel.b, pixel.g, pixel.r)
+	}
+	return
 }
